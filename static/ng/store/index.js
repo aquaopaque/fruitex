fd0f2945340b95340cc53a39e43c6c468421dbff
Dean Wang <aquaopaque@gmail.com>
2014-04-24 22:12:30 -0400

    Alter: Angularjs UI router

--- static/ng/store/index.js
+++ static/ng/store/index.js
@@ -1,1 +1,21 @@
-angular.module('store', ['store.controllers']);
+angular.module('store', ['store.controllers', 'ui.router'])
+.run(function ($rootScope, $state, $stateParams) {
+    $rootScope.$state = $state;
+    $rootScope.$stateParams = $stateParams;
+	}
+)
+.config(function($stateProvider, $urlRouterProvider) {
+  $urlRouterProvider.otherwise('');
+
+
+	$stateProvider
+	.state("faq", {
+		url: "/section/{section:\w+}",
+
+		templateUrl: "/static/ng/store/templates/qa-section.html",
+		controller: function($scope, $stateParams, $http) {
+			var faq = $scope.$parent.faq;
+			$scope.text = faq[$stateParams.section];
+		}
+	});
+});
